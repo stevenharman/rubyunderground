596833a544fc8760ede86aafee1381dbf57b038f
toddkaufman <toddkaufman@e868d9c4-fc44-0410-ad84-2ffa095e5586>
2008-01-31 04:40:49 +0000

    6/79 Array Tests completed, changed not implemented ones to Red before Green :-)

--- trunk/Tests/ArrayTests/array_test.rb
+++ trunk/Tests/ArrayTests/array_test.rb
@@ -2,19 +2,19 @@ require "test/unit"
 class ArrayTest < Test::Unit::TestCase
   
   def test_ampersand
-    
+fail("Not Yet Implemented!")
   end
   
   def test_star
-    
+fail("Not Yet Implemented!")
   end
   
   def test_plus
-    
+fail("Not Yet Implemented!")
   end
   
   def test_minus
-    
+fail("Not Yet Implemented!")
   end
   
   def test_lt_lt
@@ -25,31 +25,31 @@ class ArrayTest < Test::Unit::TestCase
   end
   
   def test_ufo
-    
+fail("Not Yet Implemented!")
   end
   
   def test_eq_eq
-    
+fail("Not Yet Implemented!")
   end
   
   def test_brackets
-    
+fail("Not Yet Implemented!")
   end
   
   def test_bracket_equals
-    
+fail("Not Yet Implemented!")
   end
   
   def test_abbrev
-    
+fail("Not Yet Implemented!")
   end
   
   def test_assoc
-    
+fail("Not Yet Implemented!")
   end
   
   def test_at
-    
+fail("Not Yet Implemented!")
   end
   
   def test_clear
@@ -60,63 +60,63 @@ class ArrayTest < Test::Unit::TestCase
   end
   
   def test_collect
-    
+fail("Not Yet Implemented!")
   end
   
   def test_collect!
-    
+fail("Not Yet Implemented!")
   end
   
   def test_compact
-    
+fail("Not Yet Implemented!")
   end
   
   def test_compact!
-    
+fail("Not Yet Implemented!")
   end
   
   def test_concat
-    
+fail("Not Yet Implemented!")
   end
   
   def test_dclone
-    
+fail("Not Yet Implemented!")
   end
   
   def test_delete
-    
+fail("Not Yet Implemented!")
   end
   
   def test_delete_at
-    
+fail("Not Yet Implemented!")
   end
   
   def test_delete_if
-    
+fail("Not Yet Implemented!")
   end
   
   def test_each
-    
+fail("Not Yet Implemented!")
   end
   
   def test_each_index
-    
+fail("Not Yet Implemented!")
   end
   
   def test_empty?
-    
+fail("Not Yet Implemented!")
   end
   
   def test_eql?
-    
+fail("Not Yet Implemented!")
   end
   
   def test_fetch
-    
+fail("Not Yet Implemented!")
   end
   
   def test_fill
-    
+fail("Not Yet Implemented!")
   end
   
   def test_first
@@ -135,51 +135,51 @@ class ArrayTest < Test::Unit::TestCase
   end
   
   def test_flatten
-    
+fail("Not Yet Implemented!")
   end
   
   def test_flatten!
-    
+fail("Not Yet Implemented!")
   end
   
   def test_frozen?
-    
+fail("Not Yet Implemented!")
   end
   
   def test_hash
-    
+fail("Not Yet Implemented!")
   end
   
   def test_include?
-    
+fail("Not Yet Implemented!")
   end
   
   def test_index
-    
+fail("Not Yet Implemented!")
   end
   
   def test_indexes
-    
+fail("Not Yet Implemented!")
   end
   
   def test_indices
-    
+fail("Not Yet Implemented!")
   end
   
   def test_initialize_copy
-    
+fail("Not Yet Implemented!")
   end
   
   def test_insert
-    
+fail("Not Yet Implemented!")
   end
   
   def test_inspect
-    
+fail("Not Yet Implemented!")
   end
   
   def test_join
-    
+fail("Not Yet Implemented!")
   end
   
   def test_last
@@ -198,83 +198,87 @@ class ArrayTest < Test::Unit::TestCase
   end
   
   def test_length
+    a = [1,2,3]
     
+    assert_equal(3, a.size, "Length did not return correct length")
+    assert_equal(0, [].size, "Length did not return 0 on an empty array")
+    assert_equal(a.length, a.size, "Length did not equal size")
   end
   
   def test_map
-    
+fail("Not Yet Implemented!")
   end
   
   def test_map!
-    
+fail("Not Yet Implemented!")
   end
   
   def test_nitems
-    
+fail("Not Yet Implemented!")
   end
   
   def test_pack
-    
+fail("Not Yet Implemented!")
   end
   
   def test_pop
-    
+fail("Not Yet Implemented!")
   end
   
   def test_pretty_print
-    
+fail("Not Yet Implemented!")
   end
   
   def test_pretty_print_cycle
-    
+fail("Not Yet Implemented!")
   end
   
   def test_push
-    
+fail("Not Yet Implemented!")
   end
   
   def test_quote
-    
+fail("Not Yet Implemented!")
   end
   
   def test_rassoc
-    
+fail("Not Yet Implemented!")
   end
   
   def test_reject
-    
+fail("Not Yet Implemented!")
   end
   
   def test_reject!
-    
+fail("Not Yet Implemented!")
   end
   
   def test_replace
-    
+fail("Not Yet Implemented!")
   end
   
   def test_reverse
-    
+fail("Not Yet Implemented!")
   end
   
   def test_reverse!
-    
+fail("Not Yet Implemented!")
   end
   
   def test_reverse_each
-    
+fail("Not Yet Implemented!")
   end
   
   def test_rindex
-    
+fail("Not Yet Implemented!")
   end
   
   def test_select
-    
+fail("Not Yet Implemented!")
   end
   
   def test_shift
-    
+fail("Not Yet Implemented!")
   end
   
   def test_size
@@ -286,67 +290,67 @@ class ArrayTest < Test::Unit::TestCase
   end
   
   def test_slice
-    
+fail("Not Yet Implemented!")
   end
   
   def test_slice!
-    
+fail("Not Yet Implemented!")
   end
   
   def test_sort
-    
+fail("Not Yet Implemented!")
   end
   
   def test_sort!
-    
+fail("Not Yet Implemented!")
   end
   
   def test_to_a
-    
+fail("Not Yet Implemented!")
   end
   
   def test_to_ary
-    
+fail("Not Yet Implemented!")
   end
   
   def test_to_s
-    
+fail("Not Yet Implemented!")
   end
   
   def test_to_yaml
-    
+fail("Not Yet Implemented!")
   end
   
   def test_transpose
-    
+fail("Not Yet Implemented!")
   end
   
   def test_uniq
-    
+fail("Not Yet Implemented!")
   end
   
   def test_uniq!
-    
+fail("Not Yet Implemented!")
   end
   
   def test_unshift
-    
+fail("Not Yet Implemented!")
   end
   
   def test_values_at
-    
+fail("Not Yet Implemented!")
   end
   
   def test_yaml_initialize
-    
+fail("Not Yet Implemented!")
   end
   
   def test_zip
-    
+fail("Not Yet Implemented!")
   end
   
   def test_bar
-    
+fail("Not Yet Implemented!")
   end
   
 end
