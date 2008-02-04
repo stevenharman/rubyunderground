df77943a9cca8d17937547ff5ae77b93e020b215
steveharman <steveharman@e868d9c4-fc44-0410-ad84-2ffa095e5586>
2008-02-04 20:10:21 +0000

    adding spec-unit

--- trunk/Tests/HashTests/hash_tests.rb
+++ trunk/Tests/HashTests/hash_tests.rb
@@ -1,213 +1,215 @@
 require "test/unit"
+require "rubygems"
+require "spec-unit"
 
 class HashTest < Test::Unit::TestCase
 
-	def test_eq_eq
-		
-	end
-	
-	def test_brackets
-		
-	end
-	
-	def test_bracket_equal
-		
-	end
-	
-	def test_clear
-		
-	end
-	
-	def test_default
-		
-	end
-	
-	def test_default_equal
-		
-	end
-	
-	def test_default_proc
-		
-	end
-	
-	def test_delete
-		
-	end
-	
-	def test_delete_if
-		
-	end
-	
-	def test_each
-		
-	end
-	
-	def test_each_key
-		
-	end
-	
-	def test_each_pair
-		
-	end
-	
-	def test_each_value
-		
-	end
-	
-	def test_empty?
-		
-	end
-	
-	def test_fetch
-		
-	end
-	
-	def test_has_key?
-		
-	end
-	
-	def test_has_value?
-		
-	end
-	
-	def test_include?
-		
-	end
-	
-	def test_index
-		
-	end
-	
-	def test_indexes
-		
-	end
-	
-	def test_indices
-		
-	end
-	
-	def test_initialize_copy
-		
-	end
-	
-	def test_inspect
-		
-	end
-	
-	def test_invert
-		
-	end
-	
-	def test_key?
-		
-	end
-	
-	def test_keys
-		
-	end
-	
-	def test_length
-		
-	end
-	
-	def test_member?
-		
-	end
-	
-	def test_merge
-		
-	end
-	
-	def test_merge!
-		
-	end
-	
-	def test_new
-		
-	end
-	
-	def test_pretty_print
-		
-	end
-	
-	def test_pretty_print_cycle
-		
-	end
-	
-	def test_rehash
-		
-	end
-	
-	def test_reject
-		
-	end
-	
-	def test_reject!
-		
-	end
-	
-	def test_replace
-		
-	end
-	
-	def test_select
-		
-	end
-	
-	def test_shift
-		
-	end
-	
-	def test_size
-		
-	end
-	
-	def test_size
-		
-	end
-	
-	def test_sort
-		
-	end
-	
-	def test_store
-		
-	end
-	
-	def test_to_a
-		
-	end
-	
-	def test_to_hash
-		
-	end
-	
-	def test_to_s
-		
-	end
-	
-	def test_to_yaml
-		
-	end
-	
-	def test_update
-		
-	end
-	
-	def test_value?
-		
-	end
-	
-	def test_values
-		
-	end
-	
-	def test_values_at
-		
-	end
-	
-	def test_yaml_initialize
-		
-	end
+  def test_eq_eq
+    
+  end
+  
+  def test_brackets
+    
+  end
+  
+  def test_bracket_equal
+    
+  end
+  
+  def test_clear
+    
+  end
+  
+  def test_default
+    
+  end
+  
+  def test_default_equal
+    
+  end
+  
+  def test_default_proc
+    
+  end
+  
+  def test_delete
+    
+  end
+  
+  def test_delete_if
+    
+  end
+  
+  def test_each
+    
+  end
+  
+  def test_each_key
+    
+  end
+  
+  def test_each_pair
+    
+  end
+  
+  def test_each_value
+    
+  end
+  
+  def test_empty?
+    
+  end
+  
+  def test_fetch
+    
+  end
+  
+  def test_has_key?
+    
+  end
+  
+  def test_has_value?
+    
+  end
+  
+  def test_include?
+    
+  end
+  
+  def test_index
+    
+  end
+  
+  def test_indexes
+    
+  end
+  
+  def test_indices
+    
+  end
+  
+  def test_initialize_copy
+    
+  end
+  
+  def test_inspect
+    
+  end
+  
+  def test_invert
+    
+  end
+  
+  def test_key?
+    
+  end
+  
+  def test_keys
+    
+  end
+  
+  def test_length
+    
+  end
+  
+  def test_member?
+    
+  end
+  
+  def test_merge
+    
+  end
+  
+  def test_merge!
+    
+  end
+  
+  def test_new
+    
+  end
+  
+  def test_pretty_print
+    
+  end
+  
+  def test_pretty_print_cycle
+    
+  end
+  
+  def test_rehash
+    
+  end
+  
+  def test_reject
+    
+  end
+  
+  def test_reject!
+    
+  end
+  
+  def test_replace
+    
+  end
+  
+  def test_select
+    
+  end
+  
+  def test_shift
+    
+  end
+  
+  def test_size
+    
+  end
+  
+  def test_size
+    
+  end
+  
+  def test_sort
+    
+  end
+  
+  def test_store
+    
+  end
+  
+  def test_to_a
+    
+  end
+  
+  def test_to_hash
+    
+  end
+  
+  def test_to_s
+    
+  end
+  
+  def test_to_yaml
+    
+  end
+  
+  def test_update
+    
+  end
+  
+  def test_value?
+    
+  end
+  
+  def test_values
+    
+  end
+  
+  def test_values_at
+    
+  end
+  
+  def test_yaml_initialize
+    
+  end
 
 end
