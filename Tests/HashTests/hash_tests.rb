ddcf48ec4afa5aefc4bd2eaaae1e6cd0a5f99049
steveharman <steven@harmanly.com>
2008-02-26 04:55:10 +0000

    Added some more BDD style tests... I think I'm ready for rspec as SpecUnit seems too limited.

--- Tests/HashTests/hash_tests.rb
+++ Tests/HashTests/hash_tests.rb
@@ -13,11 +13,15 @@ class HashTests < Test::Unit::TestCase
     
     def specify_equals_other_new_hash
       @empty_hash.should_equal(Hash.new)
+      
+      (@empty_hash == Hash.new).should_be(true)
     end
     
     def specify_doesnt_equal_hash_with_values
       h2 = { :a => "first", :b => "second" }
       @empty_hash.should_not_equal(h2)
+      
+      (@empty_hash == h2).should_be false
     end
     
     def specify_brackets_should_return_nil
@@ -28,9 +32,32 @@ class HashTests < Test::Unit::TestCase
       @empty_hash.default.should_be_nil?
     end
     
+    def specify_default_assignment_should_update_default_value
+      @empty_hash.default = :yadda
+      
+      @empty_hash.default.should_equal(:yadda)
+    end
+    
+    def specify_assignment_adds_keyvalue_to_hash
+      @empty_hash[:a] = "mine"
+      
+      @empty_hash.size.should_equal 1
+      @empty_hash[:a].should_equal("mine")
+    end
+    
+    def specify_delete_returns_nil
+      @empty_hash.delete(:b).should_be_nil?
+    end
+    
+    def specify_delete_with_block_should_execute_block_for_key
+      msg = @empty_hash.delete(:z) { |k| "#{k} wasn't found"}
+      
+      msg.should_equal "z wasn't found"
+    end
+    
   end
   
-  context 'when Hash has content and default = :foo' do
+  context 'when Hash has content and default is :foo' do
     
     def setup
       @my_hash = { :a => 'thing A', :b => 'thing B', :c => 3}
@@ -39,11 +66,15 @@ class HashTests < Test::Unit::TestCase
     
     def specify_doesnt_equal_new_hash
       @my_hash.should_not_equal(Hash.new)
+      
+      (@my_hash == Hash.new).should_be(false)
     end
     
     def specify_should_equal_hash_with_same_keyvalue_pairs
       other_hash = { :a => 'thing A', :b => 'thing B', :c => 3}
       @my_hash.should_equal(other_hash)
+      
+      (@my_hash == other_hash).should_be true
     end
     
     def specify_brackets_return_value_for_key
@@ -58,38 +89,41 @@ class HashTests < Test::Unit::TestCase
       @my_hash.default.should_equal :foo
     end
     
-  end
-
-  def test_eq_eq
+    def specify_default_assignment_should_change_value
+      @my_hash.default = 123
+      
+      @my_hash.default.should_equal 123
+    end
     
-  end
-  
-  def test_brackets
+    def specify_assignment_should_updated_existing_keyvalue_pair
+      @my_hash[:b] = 'bar'
+      
+      @my_hash.size.should_equal 3
+      @my_hash[:b].should_equal 'bar'
+      @my_hash.should_equal({ :a => 'thing A', :b => 'bar', :c => 3})
+    end
     
-  end
-  
-  def test_bracket_equal
+    def specify_clear_should_empty_the_hash
+      @my_hash.clear
+      @my_hash.should_be_empty?
+    end
     
-  end
-  
-  def test_clear
+    def specify_delete_should_return_value
+      @my_hash.delete(:c).should_equal 3
+    end
     
-  end
-  
-  def test_default
-    
-  end
-  
-  def test_default_equal
+    def specify_delete_should_remove_keyvalue_pair
+      @my_hash.delete(:b)
+      
+      @my_hash.size.should_equal 2
+      @my_hash.include?(:b).should_be false
+      @my_hash.values.should_have(2).things
+    end
     
   end
   
   def test_default_proc
-    
-  end
-  
-  def test_delete
-    
+    flunk('default_proc net tested')
   end
   
   def test_delete_if
